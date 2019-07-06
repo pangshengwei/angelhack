b6be3063b35fd53cbc3c1fef0226d8887e4161df
zulyang <zulyang94@gmail.com>
2019-07-06 19:21:48 +0800

    commit front end
    																																																		commit

---
+++ paper-dashboard-angular-master/src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { AppRoutes } from './app.routing';
+import { SidebarModule } from './sidebar/sidebar.module';
+import { FooterModule } from './shared/footer/footer.module';
+import { NavbarModule} from './shared/navbar/navbar.module';
+import { FixedPluginModule} from './shared/fixedplugin/fixedplugin.module';
+import { NguiMapModule} from '@ngui/map';
+
+import { DashboardComponent }   from './dashboard/dashboard.component';
+import { UserComponent }   from './user/user.component';
+import { TableComponent }   from './table/table.component';
+import { IconsComponent }   from './icons/icons.component';
+import { MapsComponent }   from './maps/maps.component';
+import { NotificationsComponent }   from './notifications/notifications.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    UserComponent,
+    TableComponent,
+    IconsComponent,
+    MapsComponent,
+    NotificationsComponent,
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(AppRoutes),
+    SidebarModule,
+    NavbarModule,
+    FooterModule,
+    FixedPluginModule,
+    NguiMapModule.forRoot({apiUrl: 'https://maps.google.com/maps/api/js?key=AIzaSyCBdqIhRDqVhAefF9jDhRhslerC9D-I9xM'})
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
